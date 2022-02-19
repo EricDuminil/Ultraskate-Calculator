4648d4f411dfa99056fbf6ba9ec158d553cfa30a
Eric Duminil <eric.duminil@gmail.com>
2022-02-19 11:21:25 +0100

    Seems compatible

--- ultra_app/test_new_format.py
+++ ultra_app/test_new_format.py
@@ -26,11 +26,11 @@ def get_key(event_id=EVENT_ID):
     url = f"{BASE_URL}/config.php?eventid={event_id}&page=live&noVisitor=1"
     return get_json_data(url)['key']
 
-def rider_dict(rider_id, position, _1, name, _2, laps, miles, time, diff):
+def rider_dict(rider_id, position, _id, name, _team, laps, miles, time, diff):
     return {
             'id': int(rider_id),
             'name': name,
-            'position': position,
+            'position': int(position.replace('.', '').replace('na','999')),
             'laps': int(laps),
             'time': time
         }
@@ -47,7 +47,22 @@ def find_rider(name, event_id=EVENT_ID):
     riders = get_all_riders(event_id)
     return next(r for r in riders if name in r['name'])
 
-print(find_rider(RIDER_NAME))
+def get_laps(rider, event_id=EVENT_ID):
+    key = get_key(event_id)
+    url = f"{BASE_URL}/list.php?eventid={event_id}&key={key}&listname=Online%7CLap+Details&page=live&contest=0&r=bib2&bib={rider['id']}"
+    return get_json_data(url)['data']
+
+def convert_to_racetec_format(rider):
+    table = get_laps(rider)
+    result = []
+    for row in table:
+        _id, lap, total_time, time = row
+        result.append([f'Lap {lap}', total_time, time, rider['position'], rider['position']])
+    return result
+
+rider = find_rider(RIDER_NAME)
+laps = get_laps(rider)
+r = convert_to_racetec_format(rider)
 
 
 
