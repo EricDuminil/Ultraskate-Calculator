a7db9a4e42e293c9396d0b246f17704275d579b4
Eric Duminil <eric.duminil@gmail.com>
2023-02-11 12:02:03 +0100

    Trying to show categories

--- ultra_app/__init__.py
+++ ultra_app/__init__.py
@@ -5,12 +5,12 @@ import json
 from datetime import timedelta
 
 requests_cache.install_cache('prod_cache',
-                             expire_after=60, # [s]
+                             expire_after=60,  # [s]
                              cache_control=True,
                              stale_if_error=False,
                              use_temp=True)
 
-EVENT_ID = '204047' # For Miami 2023
+EVENT_ID = '204047'  # For Miami 2023
 #EVENT_ID = '192607'
 EVENT_NAME = 'Miami Ultraskate 2023'
 RIDER_NAME = 'Mathieu'
@@ -28,46 +28,55 @@ total_ultra_time_sec = 24 * 60 * 60
 
 ################ RaceResults ##################
 
+
 def get_json_data(url):
     print(f"### Downloading {url}")
     r = requests.get(url)
     return json.loads(r.content)
 
+
 def get_key(event_id=EVENT_ID):
     url = f"{BASE_URL}/{event_id}/RRPublish/data/config?page=participants&noVisitor=1"
     return get_json_data(url)['key']
 
+
 def rider_dict(rider_id, position, _id, name, _team, laps, miles, time, diff):
     return {
-            'id': int(rider_id),
-            'name': name,
-            'position': int(position.replace('.', '').replace('na','999')),
-            'laps': int(laps),
-            'time': time
-        }
+        'id': int(rider_id),
+        'name': name,
+        'position': int(position.replace('.', '').replace('na', '999')),
+        'laps': int(laps),
+        'time': time
+    }
+
 
 def get_all_riders(event_id=EVENT_ID):
     key = get_key(event_id)
     url = f"{BASE_URL}/{event_id}/RRPublish/data/list?key={key}&listname=Result%20Lists%7COverall%20Results%20-%20Live&page=live&contest=0&r=leaders&l=100"
     table = get_json_data(url)['data']
-    categories = [cat for team_or_not in table.values() for cat in team_or_not.values()]
+    categories = [cat for team_or_not in table.values()
+                  for cat in team_or_not.values()]
     riders = [rider for riders in categories for rider in riders]
     return [rider_dict(*r) for r in riders if len(r) > 1]
+
 
 def find_rider(name, event_id=EVENT_ID):
     riders = get_all_riders(event_id)
     return next(r for r in riders if name in r['name'])
+
 
 def get_laps(rider, event_id=EVENT_ID):
     key = get_key(event_id)
     url = f"{BASE_URL}/{event_id}/RRPublish/data/list?key={key}&listname=Online%7CLap%20Details&page=live&contest=0&r=bib2&bib={rider['id']}"
     return get_json_data(url)['data']
 
+
 def fill_hours(time):
     if time.count(':') == 1:
         return '0:' + time
     else:
         return time
+
 
 def convert_to_racetec_format(rider):
     table = get_laps(rider)
@@ -84,6 +93,7 @@ def convert_to_racetec_format(rider):
 #################################################
 # Calculating the data
 
+
 def ultra_calc(input_list):
     lap_list = []
     lap_count = 0
@@ -94,7 +104,8 @@ def ultra_calc(input_list):
 
         position = single_lap[4]
 
-        laptime_second = round(float(seconds)) + (int(minutes) * 60) + (int(hours) * 3600)
+        laptime_second = round(float(seconds)) + \
+            (int(minutes) * 60) + (int(hours) * 3600)
 
         speed = (lap_distance / 0.62137119) / laptime_second * 3600
 
@@ -133,7 +144,8 @@ def calculate_output_list():
     rider = find_rider(RIDER_NAME)
     race_data_final = convert_to_racetec_format(rider)
 
-    total_miles, lap_count, position, total_elapsed_time, lap_list = ultra_calc(race_data_final)
+    total_miles, lap_count, position, total_elapsed_time, lap_list = ultra_calc(
+        race_data_final)
     results_last5 = ultra_calc(race_data_final[-5:])
     results_last = ultra_calc(race_data_final[-1:])
 
@@ -150,33 +162,37 @@ def calculate_output_list():
     total_elapsed_time_last = results_last[3]
 
     average_speed_total = average_speed(total_miles, total_elapsed_time)
-    average_speed_last5 = average_speed(total_miles_last5, total_elapsed_time_last5)
-    average_speed_last = average_speed(total_miles_last, total_elapsed_time_last)
+    average_speed_last5 = average_speed(
+        total_miles_last5, total_elapsed_time_last5)
+    average_speed_last = average_speed(
+        total_miles_last, total_elapsed_time_last)
 
     mile_per_second = average_speed_total[1]
     mile_projection = remaining_time * mile_per_second + total_miles
 
     return [
-            f"Position : {position} / Laps : {lap_count}",
-            f"Elapsed : {timedelta(seconds=total_elapsed_time)} / Remaning : {timedelta(seconds=remaining_time)}",
-            f"Mileage : {total_miles:.2f} miles / {total_km:.2f} km",
-            f"Average speed : {average_speed_total[0]:.2f} km/h",
-            f"Projection at current speed : {mile_projection:.2f} miles",
-            f"Average on last lap: {average_speed_last[0]:.2f} km/h",
-            f"Average on last 5 laps: {average_speed_last5[0]:.2f} km/h",
-            f"Average to {mileage_2020:.1f} miles : {average_speed_to_goal(mileage_2020, total_km, remaining_time):.2f} km/h / "
-            f"{remaining_lap_calc(155, lap_count)} laps",
-            f"Average to 250 miles : {average_speed_to_goal(251.12, total_km, remaining_time):.2f} km/h / "
-            f"{remaining_lap_calc(172, lap_count)} laps",
-            f"Average to {mileage_2019:.1f} miles : {average_speed_to_goal(mileage_2019, total_km, remaining_time):.2f} km/h / "
-            f"{remaining_lap_calc(180, lap_count)} laps",
-            f"Average to {mileage_2018:.1f} miles : {average_speed_to_goal(mileage_2018, total_km, remaining_time):.2f} km/h / "
-            f"{remaining_lap_calc(199, lap_count)} laps",
-            f"Average to 300 miles : {average_speed_to_goal(300, total_km, remaining_time):.2f} km/h / "
-            f"{remaining_lap_calc(206, lap_count)} laps"
-            ], lap_list
+        f"Position : {position} / Laps : {lap_count}",
+        f"Elapsed : {timedelta(seconds=total_elapsed_time)} / Remaning : {timedelta(seconds=remaining_time)}",
+        f"Mileage : {total_miles:.2f} miles / {total_km:.2f} km",
+        f"Average speed : {average_speed_total[0]:.2f} km/h",
+        f"Projection at current speed : {mile_projection:.2f} miles",
+        f"Average on last lap: {average_speed_last[0]:.2f} km/h",
+        f"Average on last 5 laps: {average_speed_last5[0]:.2f} km/h",
+        f"Average to {mileage_2020:.1f} miles : {average_speed_to_goal(mileage_2020, total_km, remaining_time):.2f} km/h / "
+        f"{remaining_lap_calc(155, lap_count)} laps",
+        f"Average to 250 miles : {average_speed_to_goal(251.12, total_km, remaining_time):.2f} km/h / "
+        f"{remaining_lap_calc(172, lap_count)} laps",
+        f"Average to {mileage_2019:.1f} miles : {average_speed_to_goal(mileage_2019, total_km, remaining_time):.2f} km/h / "
+        f"{remaining_lap_calc(180, lap_count)} laps",
+        f"Average to {mileage_2018:.1f} miles : {average_speed_to_goal(mileage_2018, total_km, remaining_time):.2f} km/h / "
+        f"{remaining_lap_calc(199, lap_count)} laps",
+        f"Average to 300 miles : {average_speed_to_goal(300, total_km, remaining_time):.2f} km/h / "
+        f"{remaining_lap_calc(206, lap_count)} laps"
+    ], lap_list
+
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -190,6 +206,31 @@ def index():
                                )
     except StopIteration:
         return "Live data not yet available."
+
+
+def get_all_riders_and_categories(event_id=EVENT_ID):
+    key = get_key(event_id)
+    url = f"{BASE_URL}/{event_id}/RRPublish/data/list?key={key}&listname=Participants%7CIndividual%20Skaters&page=participants&contest=0&r=all&l=0"
+    table = get_json_data(url)['data']
+    return [x for row in table.values() for x in row]
+
+
+@app.route("/categories")
+def categories():
+    data = get_all_riders_and_categories()
+    riders = get_all_riders()
+    import pandas as pd
+    df2 = pd.DataFrame(riders)
+    df = pd.DataFrame(data, columns=['id', 'bib2', 'x', 'name', 'team_name',
+                      'age', 'discipline', 'gender', 'clydesdale', 'nation'])
+
+    df = df[['id', 'name', 'age', 'discipline', 'gender', 'clydesdale', 'nation']]
+    df = df.set_index('id')
+    df2 = df2.set_index('id')
+    print(df2)
+
+    print(df)
+    return repr(df)
 
 
 if __name__ == "__main__":
